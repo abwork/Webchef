573cbe9c3230e396a27c795a1003cf85fdc7aff3
Abdi Muleta <abdi.muleta@integrify.io>
2018-08-06 22:26:21 +0300

    localstorage commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import './App.css';
 import Search from './components/Search';
 import Recipes from './components/Recipes';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const API_KEY = "619791493c6727577559c260ba548e46";
 // const URL = "https://cors-anywhere.herokuapp.com/http://food2fork.com/api/search?key=619791493c6727577559c260ba548e46&q=shredded%20chicken&count=6";
@@ -27,23 +29,39 @@ class App extends Component {
   //         console.log(this.state.recipes);
   // }
 
+  //fetch recipes and store in state
   getRecipe = async (e) => {
     const recipeName = e.target.elements.recipeName.value;
     e.preventDefault();
-    const api_call = await fetch(`https://cors-anywhere.herokuapp.com/http://food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=6`);
+    const api_call = await fetch(`https://cors-anywhere.herokuapp.com/http://food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=15`);
     const data = await api_call.json();
     this.setState({ recipes: data.recipes });
     console.log(this.state.recipes);
   }
-  
+
+  //fetch data from the localstorage
+  componentDidMount = () => {
+    const data = localStorage.getItem("recipes");
+    const recipes = JSON.parse(data);
+    this.setState({
+      recipes
+    });
+  }
+
+  //save recipes to local storage 
+  //to restore recipes data when we come back from the recipe view
+  componentDidUpdate = () => {
+    const recipes = JSON.stringify(this.state.recipes);
+    localStorage.setItem("recipes", recipes);
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Webchef</h1>
-        </header>
+        <Header />
         <Search getRecipe={this.getRecipe} />
         <Recipes recipes={this.state.recipes}/>
+        <Footer />
       </div>
     );
   } 
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => (
+
+    <footer className="App-footer">
+        <p className="App-author">Abdi Muleta - 2018</p>
+    </footer>
+);
+
+export default Footer;
+
